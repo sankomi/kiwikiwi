11de9a6dc2b06921cf37f1e5154648c4894536be
sanko <sang.koh@hotmail.com>
2023-10-31 21:03:34 +1100

    change to replace/fake and more tests

--- javascript/test/wiki.js
+++ javascript/test/wiki.js
@@ -1,5 +1,6 @@
 const assert = require("assert").strict;
 const sinon = require("sinon");
+const {replace, fake, restore} = sinon;
 
 const wiki = require("../wiki");
 const {Page} = require("../models");
@@ -17,10 +18,10 @@ describe("wiki.js", function() {
 			let title = "viewnopage";
 
 			it("should return not-exist view with title", async function() {
-				sinon.stub(Page, "findOne").returns(null);
-				sinon.stub(Page, "build").returns({title});
+				let findOne = replace(Page, "findOne", fake(args => null));
 
 				let view = await wiki.view(title);
+				assert.equal(findOne.callCount, 1);
 				assert.equal(view.name, "not-exist");
 				assert.deepEqual(view.data, {page: {title}});
 			});
@@ -31,11 +32,43 @@ describe("wiki.js", function() {
 			let page = {title};
 
 			it("should return view view with the page", async function() {
-				sinon.stub(Page, "findOne").returns(page);
+				let findOne = replace(Page, "findOne", fake(args => page));
 
 				let view = await wiki.view(title);
+				assert.equal(findOne.callCount, 1);
 				assert.equal(view.name, "view");
 				assert.deepEqual(view.data, {page});
+			});
+		});
+	});
+
+	describe("editView(title)", function() {
+		describe("if page does not exist", function() {
+			let title = "editviewnopage";
+
+			it("should return edit view with title and new title", async function() {
+				let findOne = replace(Page, "findOne", fake(args => null));
+				let build = replace(Page, "build", fake(args => ({title: args.title})));
+
+				let view = await wiki.editView(title);
+				assert.equal(findOne.callCount, 1);
+				assert.equal(build.callCount, 1);
+				assert.equal(view.name, "edit");
+				assert.deepEqual(view.data, {page: {title, newTitle: title}});
+			});
+		});
+
+		describe("if page exists", function() {
+			let title = "editviewpage";
+			let page = {title};
+
+			it("should return edit view with the page", async function() {
+				let findOne = replace(Page, "findOne", fake(args => page));
+
+				let view = await wiki.editView(title);
+				assert.equal(findOne.callCount, 1);
+				assert.equal(view.name, "edit");
+				assert.deepEqual(view.data, {page: {...page, newTitle: title}});
 			});
 		});
 	});
